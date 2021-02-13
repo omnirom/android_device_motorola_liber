4a03f01d9c45c9680fb3dce20f3684407f4eaafe
Vachounet <vachounet@live.fr>
2021-02-13 18:26:20 +0530

    liber: light: Clean up
    Change-Id: Ifd03b6e3baaa665357d3933b3685097b26f1d5ab

--- lights/Light.cpp
+++ lights/Light.cpp
@@ -43,13 +43,11 @@ namespace implementation {
  * Write value to path and close file.
  */
 static void set(std::string path, std::string value) {
-    ALOGE("set string");
     std::ofstream file(path);
     file << value;
 }
 
 static void set(std::string path, int value) {
-    ALOGE("set int");
     set(path, std::to_string(value));
 }
 
@@ -65,17 +63,12 @@ static T get(const std::string& path, const T& def) {
 static void handleBacklight(const LightState& state) {
     int maxBrightness = get("/sys/class/backlight/panel0-backlight/max_brightness", -1);
     if (maxBrightness < 0) {
-        maxBrightness = 255;
+        maxBrightness = 4096;
     }
     int sentBrightness = Light::rgbToBrightness(state);
     int brightness = sentBrightness * maxBrightness / 255;
     set(LCD_LED, brightness);
     set(LCD_LED, brightness);
-}
-
-static void handleBattery(const LightState& state) {
-    int brightness = Light::rgbToBrightness(state);
-    set(LEDS BRIGHTNESS, brightness);
 }
 
 static void handleNotification(const LightState& state) {
@@ -100,7 +93,6 @@ static void handleNotification(const LightState& state) {
 
 static std::map<Type, std::function<void(const LightState&)>> lights = {
     {Type::BACKLIGHT, handleBacklight},
-    {Type::BATTERY, handleBattery},
     {Type::NOTIFICATIONS, handleNotification},
     {Type::ATTENTION, handleNotification},
 };
